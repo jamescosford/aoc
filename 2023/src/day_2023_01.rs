8ffd90e0ced7e1e49f175999128a2b652b0bde7d
James Cosford <jamescosford@gmail.com>
2023-12-02 00:03:27 +0100

    day_2023_01 tidied

--- 2023/src/day_2023_01.rs
+++ 2023/src/day_2023_01.rs
@@ -3,7 +3,7 @@
 pub mod day_2023_01 {
   use crate::common::common::read_lines;
   use itertools::Itertools;
-use regex::Regex;
+  use regex::Regex;
   use std::{collections::HashMap, str::FromStr};
 
   pub fn aoc_a() -> i32 {
@@ -12,90 +12,20 @@ use regex::Regex;
     read_lines("input/day_2023_01.txt")
       .iter()
       .map(|f| {
-        let r: String = f.clone().chars().rev().collect();
+        let caps = re.captures_iter(f).collect_vec();
 
-        let df = re.captures_iter(f).next().map(|c| {
+        let df = caps.first().map(|c| {
           let c1 = &c[1];
           c1.parse::<i32>().unwrap()
         }).unwrap();
         
-        let dr = re.captures_iter(r.as_str()).next().map(|c| {
+        let dr = caps.last().map(|c| {
           let c1 = &c[1];
           c1.parse::<i32>().unwrap()
         }).unwrap();
 
         df*10 + dr
       }).fold(0, |acc, v| acc + v)
-  }
-
-  pub fn aoc_c() -> i32 {
-
-    let lut_f = HashMap::from([
-      ("1", 1),
-      ("2", 2),
-      ("3", 3),
-      ("4", 4),
-      ("5", 5),
-      ("6", 6),
-      ("7", 7),
-      ("8", 8),
-      ("9", 9),
-      ("one", 1),
-      ("two", 2),
-      ("three", 3),
-      ("four", 4),
-      ("five", 5),
-      ("six", 6),
-      ("seven", 7),
-      ("eight", 8),
-      ("nine", 9),
-    ]);
-    let lut_r = HashMap::from([
-      ("1", 1),
-      ("2", 2),
-      ("3", 3),
-      ("4", 4),
-      ("5", 5),
-      ("6", 6),
-      ("7", 7),
-      ("8", 8),
-      ("9", 9),
-      ("eno", 1),
-      ("owt", 2),
-      ("eerht", 3),
-      ("ruof", 4),
-      ("evif", 5),
-      ("xis", 6),
-      ("neves", 7),
-      ("thgie", 8),
-      ("enin", 9),
-    ]);
-
-    let re_f = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine)").unwrap();
-    let re_r = Regex::new(r"(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)").unwrap();
-
-    read_lines("input/day_2023_01.txt")
-    .iter()
-    .map(|f| {
-      print!("{} ", f);
-
-      let r: String = f.clone().chars().rev().collect();
-
-      let df = re_f.captures_iter(f).next().map(|c| {
-        print!("{}:", &c[1]);
-        lut_f.get(&c[1]).unwrap()
-      }).unwrap();
-      
-      let dr = re_r.captures_iter(r.as_str()).next().map(|c| {
-        print!("{}", &c[1].chars().rev().collect::<String>());
-        lut_r.get(&c[1]).unwrap()
-      }).unwrap();
-
-      println!("");
-
-      df*10 + dr
-    }).fold(0, |acc, v| acc + v)
-
   }
 
   pub fn aoc_b() -> i32 {
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,9 +1,9 @@
 mod common;
 mod day_2023_01;
 
-use crate::day_2023_01::day_2023_01::{aoc_a, aoc_b, aoc_c};
+use crate::day_2023_01::day_2023_01::{aoc_a, aoc_b};
 fn main() {
-    // println!("{}", aoc_a());
+    println!("{}", aoc_a());
     println!("{}", aoc_b());
     // aoc_b();
 }
